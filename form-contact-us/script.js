a6e8af11719ed16f326189bf24d2aaa759dcb9c5
Dzmitry <dimaau1@gmail.com>
2024-04-08 10:58:42 +0300

    contact us form

--- form-contact-us/script.js
+++ form-contact-us/script.js
@@ -6,6 +6,7 @@ document
     const name = document.getElementById("name").value.trim();
     const email = document.getElementById("email").value.trim();
     const message = document.getElementById("message").value.trim();
+
     const errorMessage = document.getElementById("errorMessage");
 
     errorMessage.innerHTML = "";
@@ -15,24 +16,10 @@ document
       return false;
     }
 
-    if (!isValidEmail(email)) {
-      errorMessage.innerHTML = "Please enter a valid email address";
-      return false;
-    }
+    //TODO: check email
 
     //send request
-    //wait for reponse
-
+    //wait for response
     document.querySelector(".message-container").classList.add("visible");
     document.querySelector(".form-container").classList.remove("visible");
   });
-
-function isValidEmail(email) {
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
-
-document.addEventListener("click", () => {
-  document.querySelector(".message-container").classList.remove("visible");
-  document.querySelector(".form-container").classList.add("visible");
-});
