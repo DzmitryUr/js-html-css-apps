44f1fab1617ffff9b307dec71f3aadeaa3143dfc
Dzmitry <dimaau1@gmail.com>
2024-04-12 13:26:11 +0300

    add portfolio projects

---
+++ script.js
@@ -0,0 +1,79 @@
+const data = [
+  {
+    name: "Counter App",
+    link: "counter/index.html",
+    image: "counter/Counter.png",
+    description: "DOM Manipulation, Events Handling",
+    youtube: "https://youtu.be/IfHdXb3Gxe0",
+    github: "https://github.com/DzmitryUr/js-html-css-apps/tree/main/counter",
+  },
+  {
+    name: "Facts App",
+    link: "facts-api/index.html",
+    image: "facts-api/Facts-API.png",
+    description: "Fetch API, Promises",
+    youtube: "https://youtu.be/ZrOqRrcpsEs",
+    github: "https://github.com/DzmitryUr/js-html-css-apps/tree/main/facts-api",
+  },
+  {
+    name: "Jokes App",
+    link: "jokes-app/index.html",
+    image: "jokes-app/Jokes-APP.png",
+    description: "Fetch API, Async, Await",
+    youtube: "https://youtu.be/b7Cgpu541lQ",
+    github: "https://github.com/DzmitryUr/js-html-css-apps/tree/main/jokes-app",
+  },
+  {
+    name: "Image Gallery",
+    link: "image-gallery/index.html",
+    image: "image-gallery/Image-Gallery-JavaScript.png",
+    description: "DOM Manipulation, Events Handling",
+    youtube: "https://youtu.be/pAc1hW3eKr0",
+    github:
+      "https://github.com/DzmitryUr/js-html-css-apps/tree/main/image-gallery",
+  },
+  {
+    name: "Accordion",
+    link: "accordion/index.html",
+    image: "accordion/Accordion Component JS.png",
+    description: "DOM Manipulation, Events Handling",
+    youtube: "https://youtu.be/81q_kEgbNPA",
+    github:
+      "https://github.com/DzmitryUr/js-html-css-apps/tree/main/image-gallery",
+  },
+  {
+    name: "Native Popups",
+    link: "native-popups/index.html",
+    image: "native-popups/Native-PopUps.png",
+    description: "Alert, Confirm, Prompt Popups",
+    youtube: "https://youtu.be/mqYTk7XoRBs",
+    github:
+      "https://github.com/DzmitryUr/js-html-css-apps/tree/main/native-popups",
+  },
+  {
+    name: "Timer App",
+    link: "timer/index.html",
+    image: "timer/Timer.png",
+    description: "Date object, setInterval, clearInterval",
+    youtube: "https://youtu.be/9nMLgtFKfNc",
+    github: "https://github.com/DzmitryUr/js-html-css-apps/tree/main/timer",
+  },
+  {
+    name: "Contact Us Form",
+    link: "form-contact-us/index.html",
+    image: "formform-contact-us/Contact-Us-Form.png",
+    description: "Form, Input, Textarea, Validation",
+    youtube: "https://youtu.be/x2ItMNJI-rc",
+    github:
+      "https://github.com/DzmitryUr/js-html-css-apps/tree/main/form-contact-us",
+  },
+  {
+    name: "Memory Words Game",
+    link: "memory-words-game/index.html",
+    image: "memory-words-game/MemoryWordsGameJS.png",
+    description: "CSS Flexbox Layout, Rem Measurement",
+    youtube: "https://youtu.be/oZpgtx6T6MY",
+    github:
+      "https://github.com/DzmitryUr/js-html-css-apps/tree/main/memory-words-game",
+  },
+];
